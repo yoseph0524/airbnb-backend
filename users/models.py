5c73c9605d7685ed17dcfa135de0301e5f3624de
yoseph0524 <yoseph0524@g.ucla.edu>
2023-09-20 20:04:10 -0700

    User model

--- users/models.py
+++ users/models.py
@@ -3,7 +3,43 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    first_name = models.CharField(max_length=150, editable=False)
-    last_name = models.CharField(max_length=150, editable=False)
-    name = models.CharField(max_length=150, default="")
-    is_host = models.BooleanField(default=False)
+    class GenderChocies(models.TextChoices):
+        MALE = ("male", "Male")
+        FEMALE = ("female", "Female")
+
+    class LanguangeChoices(models.TextChoices):
+        KR = ("kr", "Korean")
+        EN = ("en", "English")
+
+    class CurrencyChoices(models.TextChoices):
+        WON = "won", "Korean Won"
+        USD = "usd", "Dollar"
+
+    first_name = models.CharField(
+        max_length=150,
+        editable=False,
+    )
+    last_name = models.CharField(
+        max_length=150,
+        editable=False,
+    )
+    avatar = models.ImageField(blank=True)
+    name = models.CharField(
+        max_length=150,
+        default="",
+    )
+    is_host = models.BooleanField(
+        default=False,
+    )
+    gender = models.CharField(
+        max_length=10,
+        choices=GenderChocies.choices,
+    )
+    language = models.CharField(
+        max_length=2,
+        choices=LanguangeChoices.choices,
+    )
+    currency = models.CharField(
+        max_length=5,
+        choices=CurrencyChoices.choices,
+    )
