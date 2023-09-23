5cba4549102648ca82a37d5ffce287561256571b
yoseph0524 <yoseph0524@g.ucla.edu>
2023-09-23 03:47:04 -0700

    serializers.save

--- categories/serializers.py
+++ categories/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
+from .models import Category
 
 
 class CategorySerializer(serializers.Serializer):
-    pk = serializers.IntegerField()
-    name = serializers.CharField(required=True)
-    kind = serializers.CharField()
-    created_at = serializers.DateTimeField()
+    pk = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(
+        required=True,
+        max_length=50,
+    )
+    kind = serializers.ChoiceField(
+        choices=Category.CategoryKindChoices.choices,
+    )
+    created_at = serializers.DateTimeField(read_only=True)
+
+    def create(self, validated_data):
+        return Category.objects.create(**validated_data)
--- categories/views.py
+++ categories/views.py
@@ -11,11 +11,14 @@ def categories(request):
         serializer = CategorySerializer(all_categories, many=True)
         return Response(serializer.data)
     elif request.method == "POST":
-        Category.objects.create(
-            name=request.data["name"],
-            kind=request.data["kind"],
-        )
-        return Response({"created": True})
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            new_category = serializer.save()
+            return Response(
+                CategorySerializer(new_category).data,
+            )
+        else:
+            return Response(serializer.errors)
 
 
 @api_view()
