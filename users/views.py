c16e500d0fd7dc31a6abf82367f0772ffcddef0c
yoseph0524 <yoseph0524@g.ucla.edu>
2023-09-25 21:54:58 -0700

    Create User

--- users/views.py
+++ users/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.exceptions import ParseError
+from rest_framework.permissions import IsAuthenticated
+from . import serializers
 
-# Create your views here.
+
+class Me(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        serializer = serializers.PrivateUserSerializer(user)
+        return Response(serializer.data)
+
+    def put(self, request):
+        user = request.user
+        serializer = serializers.PrivateUserSerializer(
+            user,
+            data=request.data,
+            partial=True,
+        )
+        if serializer.is_valid():
+            user = serializer.save()
+            serializer = serializers.PrivateUserSerializer(user)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+class Users(APIView):
+    def post(self, request):
+        password = request.data.get("password")
+        if not password:
+            return ParseError
+        serializer = serializers.PrivateUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            user.set_password(password)
+            user.save()
+            serializer = serializers.PrivateUserSerializer(user)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+class PublicUser(APIView):
+    pass
+
+
+class ChangePassword(APIView):
+    pass
+
+
+class LogIn(APIView):
+    pass
+
+
+class LogOut(APIView):
+    pass
