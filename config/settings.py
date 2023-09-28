8c66c4b0d869a44b80af8d52877e2f1049fe6a94
yoseph0524 <yoseph0524@g.ucla.edu>
2023-09-27 23:25:55 -0700

    .

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,8 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY", default="asdfasdfasdfasdf")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "RENDER" not in os.environ
